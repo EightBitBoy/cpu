59940663bd5643e29331e620863449edd46974d1
Stefan Haertel <haertel.dev@gmail.com>
2015-06-11 23:04:45 +0200

    Play around

---
+++ Assets/cubemover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubemover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("foo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+	}
+}
