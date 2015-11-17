72237b9a8799b79ef4680d9aacd029916d158210
Stefan Haertel <haertel.dev@gmail.com>
2015-11-17 12:50:34 +0100

    Playing around

--- game.gd
+++ game.gd
@@ -6,7 +6,11 @@ extends Panel
 # var b="textvar"
 
 func _ready():
-	# Initialization here
-	pass
+	get_node("Button").connect("pressed", self, "_on_button_pressed")
+	get_node("Button").connect("released", self, "_on_button_released")
 
+func _on_button_pressed():
+	get_node("Label").set_text("FooBar")
 
+func _on_button_released():
+	get_node("Label").set_text("Woooosh")
